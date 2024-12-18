a1ebf7ae53efe130909c9a5823a55cf7c3d72a33
singjc <justincsing@gmail.com>
2024-12-17 23:54:56 -0500

    update: app.py

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
 import streamlit as st
 from pathlib import Path
+
 # For some reason the windows version only works if this is imported here
 import pyopenms
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+
     pages = {
-        "OpenMS Web App" : [
+        "StreamSage Web App": [
             st.Page(Path("content", "quickstart.py"), title="Quickstart", icon="👋"),
-            st.Page(Path("content", "documentation.py"), title="Documentation", icon="📖"),
+            st.Page(
+                Path("content", "documentation.py"), title="Documentation", icon="📖"
+            ),
         ],
-        "TOPP Workflow Framework": [
-            st.Page(Path("content", "topp_workflow_file_upload.py"), title="File Upload", icon="📁"),
-            st.Page(Path("content", "topp_workflow_parameter.py"), title="Configure", icon="⚙️"),
-            st.Page(Path("content", "topp_workflow_execution.py"), title="Run", icon="🚀"),
-            st.Page(Path("content", "topp_workflow_results.py"), title="Results", icon="📊"),
+        "Proteomics Database Search": [
+            st.Page(Path("content", "sageworkflow.py"), title="Sage", icon="🚀"),
         ],
-        "pyOpenMS Workflow" : [
-            st.Page(Path("content", "file_upload.py"), title="File Upload", icon="📂"),
-            st.Page(Path("content", "raw_data_viewer.py"), title="View MS data", icon="👀"),
-            st.Page(Path("content", "run_example_workflow.py"), title="Run Workflow", icon="⚙️"),
-            st.Page(Path("content", "download_section.py"), title="Download Results", icon="⬇️"),
-        ],
-        "Others Topics": [
-            st.Page(Path("content", "simple_workflow.py"), title="Simple Workflow", icon="⚙️"),
-            st.Page(Path("content", "run_subprocess.py"), title="Run Subprocess", icon="🖥️"),
-        ]
     }
 
     pg = st.navigation(pages)
